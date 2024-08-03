02bdc95ee834597a10d8ae057e7fa26978b469b0
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-08-03 13:20:34 +0530

    improved Ui and did responsive to header

--- frontend/src/Components/Home_Section/Team.jsx
+++ frontend/src/Components/Home_Section/Team.jsx
@@ -5,266 +5,214 @@ import chairmanImg from "../../assets/chairman.png";
 const Team = () => {
   return (
     <div className="mt-[30px] flex flex-col items-center">
-          <div className="w-[90%]">
-    <h1 className="text-center text-heading3 lg:text-heading2 font-bold mb-4 p-5 text-peacock-green relative hover:text-peacock-green-hover  transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
-        Our core team
-      </h1>
-    </div>
+      <div className="w-[90%]">
+        <h1 className="text-center text-heading3 lg:text-heading2 font-bold mb-4 p-5 text-peacock-green relative hover:text-peacock-green-hover  transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+          Our core team
+        </h1>
+      </div>
       <div className="mt-[30px] team-cards flex flex-col items-center md:flex-row md:flex-wrap md:w-[80%] md:gap-x-[30px]">
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+
+          <div className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+
+          <div
+            className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+
+          <div
+            className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+
+          <div
+            className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+          <div
+            className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+
+          <div
+            className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px] border-2 transition-transform transform hover:scale-90"
+        >
+          <div
+            className="w-[200px] h-[200px]  rounded-full "
+            style={{
+              backgroundImage: `url(${chairmanImg})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="flex flex-col items-center ">
+            <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
+              TARUN MISRA
+            </span>
+            <p className="text-[14px] font-workSans">
+              Chairman at{" "}
+              <a
+                href="https://sobf.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-[#4a8bed]"
+              >
+                SOBF
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
-        </div>
-      </div>
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
-        </div>
-      </div>
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
-        </div>
-      </div>
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
-        </div>
-      </div>
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
-        </div>
-      </div>
-      <div
-        className="mt-[20px] card flex flex-col items-center rounded-lg bg-seashell p-[25px] pb-[20px]"
-        style={{
-          boxShadow: `
-            7px 7px 15px rgba(0, 0, 0, 0.15), 
-            -7px -7px 15px rgba(255, 255, 255, 0.7)
-          `,
-        }}
-      >
-        
-        <div
-          className="w-[200px] h-[200px]  rounded-full "
-          style={{
-            backgroundImage: `url(${chairmanImg})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center ">
-          <span className="font-sans text-blue font-bold mt-[10px] text-[18px] ">
-            TARUN MISRA
-          </span>
-          <p className="text-[14px] font-workSans">
-            Chairman at{" "}
-            <a
-              href="https://sobf.in"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cursor-pointer text-[#4a8bed]"
-            >
-              SOBF
-            </a>
-          </p>
-        </div>
-      </div>
-      </div>
-      
+
     </div>
   );
 };
