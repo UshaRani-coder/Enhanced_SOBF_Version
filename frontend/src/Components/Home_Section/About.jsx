41e68da2fddd1e4052c67d891df6cbe28d4ead30
UshaRani-coder <ur66026@gmail.com>
2024-08-08 19:35:53 +0530

    Re-design of About section in Home page

--- frontend/src/Components/Home_Section/About.jsx
+++ frontend/src/Components/Home_Section/About.jsx
@@ -1,39 +1,51 @@
 import React from "react";
 import ReactPlayer from "react-player";
-import aboutImg from '../../assets/about-img.png'
-
-
+import aboutImg from "../../assets/Sobf Images/children_activities/ca17.jpg";
+import aboutBackgroundImg from "../../assets/Sobf Images/child_education_and_empowerment/cee12.jpg";
 const About = () => {
   return (
-    <div className="flex flex-col items-center lg:mx-[20px] mt-5">
-          <div className="w-[90%]">
-    <h1 className="text-center text-heading3 lg:text-heading2 font-bold mb-1 p-5 text-peacock-green relative hover:text-peacock-green-hover transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+    <div
+      className="home-about relative flex flex-col items-center "
+      style={{
+        backgroundImage: `url(${aboutBackgroundImg})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <h1 className="text-center text-[25px] md:text-heading3 lg:text-heading2 font-bold  p-5 text-peacock-green relative z-10 hover:text-peacock-green-hover transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
         Who are we?
       </h1>
-    </div>
-      <div className="mx-4 px-16">
-        <p className="text-lg text-gray-700 text-start font-medium mt-4 ">
-          <span className="text-2xl text-violet-700 font-bold hover:underline">
+      <div className="flex flex-col items-center text-center lg:text-start lg:flex lg:flex-row lg:justify-center lg:items-center lg:gap-x-[20px] z-40  mt-5 lg:px-[40px] lg:pb-[50px]">
+        <p className=" leading-[27px] text-heading5 md:text-[18px] lg:text-[15px] lg:leading-[24px] text-[rgba(225,225,225,0.85)]   mt-4 mx-4 lg:m-0 ">
+          <span className="text-xl text-violet-700 font-bold hover:underline">
             Soul Of Braj Federation
-          </span> is a Section-8 Recognized Non Profit Organization, dedicated to Serve
-          Society members in Shri Vrindavan Dham, Since 2020, SOBF is Dedicated
-          to Serve the Poorest of the Poor Residents of Vrindavan and the Braj
-          region of Uttar Pradesh, ( Bharat ). Particularly Adolescent Girls and
-          Women by Providing Comprehensive, Essential Community Services. Our
-          Initiatives are, Clean and Healthy Vrindavan, Affordable Food ,
+          </span>{" "}
+          is a Section-8 recognized Non Profit Organization, dedicated to serve
+          society members in Shri Vrindavan Dham, since 2020, SOBF is dedicated
+          to serve the poorest of the poor residents of vrindavan and the braj
+          region of Uttar Pradesh, ( Bharat ). Particularly adolescent girls and
+          women by providing comprehensive, essential community services. Our
+          initiatives are, Clean and Healthy Vrindavan, Affordable Food ,
           Providing Basic Education, Skill Training, Health-Hygiene Care,
           Distribution of free Meals / Ration Kits ,Waste Management Projects,
           Currently we are Impacting more than 1000+ Beneficiaries on Daily
           Basis.
           <br />
-          <span className=" text-violet-700 font-bold text-xl ">
-            Focused And On Going Initiatives :
-          </span> Gopala Bhog ( Affordable Food Prasadam For All ), Swasth aur Swachh
+          <div className="text-violet-700 mt-[20px] lg:mt-0 font-bold text-xl">
+            Focused And Ongoing Initiatives :
+          </div>{" "}
+          Gopala Bhog (affordable food prasadam for all), Swasth aur Swachh
           Vrindavan, Give Me A Chance, Say Yes To Me, Brajkulam Community
           Centre.
         </p>
+
+        <img
+          src={aboutImg}
+          alt="about"
+          className="z-40 my-[30px] lg:my-0 w-[95%] lg:w-[40%] lg:h-[300px] xl:h-[280px] xl:pt-[25px]"
+        />
       </div>
-      {/* <img src={aboutImg} alt="about" className="mt-[15px] w-[90%] lg:w-[95%]" /> */}
     </div>
   );
 };
