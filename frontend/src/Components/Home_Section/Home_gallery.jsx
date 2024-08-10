9960c9a797839d891632e2a7038d3d39b4f2a43b
Sriram Kashyap <kssriramkashyap002@gmail.com>
2024-08-10 20:03:53 +0530

    fixed donate page hero

--- frontend/src/Components/Home_Section/Home_gallery.jsx
+++ frontend/src/Components/Home_Section/Home_gallery.jsx
@@ -9,12 +9,13 @@ const Home_Gallery = () => {
 
   return (
     <div className="pt-[110px] py-6 border-b">
-      <div className="container mx-auto p-0 px-4 lg:px-20">
-        <div className="w-full">
-          <h1 className="text-center text-heading3 lg:text-heading2 font-bold mb-4 p-5 text-peacock-green">
+      <div className="w-full">
+          <h1 className="text-center text-heading3 lg:text-heading2 font-bold mb-4 p-5 text-peacock-green relative hover:text-peacock-green-hover transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
             Featured Gallery
           </h1>
         </div>
+      <div className="container mx-auto p-0 px-4 lg:px-20">
+        
         {/* Gallery Grid */}
         <div>
           
--- frontend/src/Components/Home_Section/Services.jsx
+++ frontend/src/Components/Home_Section/Services.jsx
@@ -79,7 +79,7 @@ const Services = () => {
   }, []);
 
   return (
-    <div className="w-[100%] flex flex-col justify-center items-center  ">
+    <div className="w-[100%] flex flex-col justify-center items-center">
       <h1 className="text-center text-[25px] md:text-heading3 lg:text-heading2 font-bold  p-5 text-peacock-green relative z-10 hover:text-peacock-green-hover transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
         Our Services
       </h1>
