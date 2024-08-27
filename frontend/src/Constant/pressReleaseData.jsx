92973b13eed5036968b328cf3e7e539221d34642
UshaRani-coder <ur66026@gmail.com>
2024-08-27 13:39:15 +0530

    updated the dummy text in Recent Activities and Press Release

--- frontend/src/Constant/pressReleaseData.jsx
+++ frontend/src/Constant/pressReleaseData.jsx
@@ -1,20 +1,24 @@
 // dummy data
-import news1 from '../assets/Sobf Images/Press Release/news1.jpg'
-import news2 from '../assets/Sobf Images/Press Release/news2.jpg'
+import news1 from "../assets/Sobf Images/Press Release/news1.jpg";
+import news2 from "../assets/Sobf Images/Press Release/news2.jpg";
 const News = [
-    {
-        media: news2,
-        date: "August 23, 2024",
-        title: "Soul of Braj Federation becomes partner in welfare work with Raturi Foundation and The Hindu Diaspora Foundation",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras vehicula, nisl ut aliquet fermentum, metus nisl facilisis nunc, ac pharetra felis.",
-        button: "Read More"
-    },
-    {
-        media: news1,
-        date: "August 23, 2024",
-        title: "The institutions providing food, education, and health services have taken new initiatives for the betterment of Braj.",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras vehicula, nisl ut aliquet fermentum, metus nisl facilisis nunc, ac pharetra felis.",
-        button: "Read More"
-    },
-]
-export default News
+  {
+    media: news2,
+    date: "August 23, 2024",
+    title:
+      "Soul of Braj Federation becomes partner in welfare work with Raturi Foundation and The Hindu Diaspora Foundation",
+    description:
+      "The Soul of Braj Federation is proud to announce its partnership with the Raturi Foundation and The Hindu Diaspora Foundation, joining forces to enhance welfare initiatives in the Braj region. This collaboration aims to amplify the impact of ongoing efforts in providing essential services like clean and healthy environments, affordable food, basic education, and skill training to the underserved communities of Vrindavan and surrounding areas.",
+    button: "Read More",
+  },
+  {
+    media: news1,
+    date: "July 20, 2024",
+    title:
+      "The institutions providing food, education, and health services have taken new initiatives for the betterment of Braj.",
+    description:
+      "Institutions dedicated to providing food, education, and health services in Braj have launched new initiatives aimed at furthering the well-being of the region's residents. These efforts focus on enhancing the quality and accessibility of essential services, ensuring that the underserved communities of Braj receive the support they need to thrive. With a renewed commitment to improving living conditions, these initiatives are set to make a significant impact in Vrindavan.",
+    button: "Read More",
+  },
+];
+export default News;
--- frontend/src/Constant/recentActivities.jsx
+++ frontend/src/Constant/recentActivities.jsx
@@ -3,16 +3,16 @@ import activity2Img from '../assets/banner2.jpeg'
 const recentActivities = [
     {
         media:activity1Img,
-        date: "August 23, 2024",
-        title: "Soul of Braj Federation becomes partner in welfare work with Raturi Foundation and The Hindu Diaspora Foundation",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras vehicula, nisl ut aliquet fermentum, metus nisl facilisis nunc, ac pharetra felis.",
+        date: "June 24, 2024",
+        title: "Soul of Braj: A Sacred Celebration of Unity and Devotion",
+        description:"The Soul of Braj Federation invites you to join in a sacred celebration of unity and devotion, honoring the rich cultural and spiritual heritage of Braj. This holy event will bring together the community in a shared experience of worship, music, and traditional rituals, celebrating the divine essence that flows through Vrindavan and its surroundings. ",
         button: "Read More"
     },
     {
         media:activity2Img,
-        date: "August 23, 2024",
-        title: "Soul of Braj Federation becomes partner in welfare work with Raturi Foundation and The Hindu Diaspora Foundation",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras vehicula, nisl ut aliquet fermentum, metus nisl facilisis nunc, ac pharetra felis.",
+        date: "January 25, 2024",
+        title: "Brajkulam Educational Center: Nurturing Minds, Shaping Futures",
+        description:"The Brajkulam Educational Center is dedicated to nurturing young minds and shaping the future of the Braj region. This center provides quality education and skill training to children and youth, empowering them with the knowledge and tools needed to thrive in today’s world. ",
         button: "Read More" 
     }
 ]
