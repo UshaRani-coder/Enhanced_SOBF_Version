2bef677a6850418a086795ac98c01edf6c5d66f8
rahuls49 <rahulsahu79998@gmail.com>
2025-01-17 20:14:44 +0530

    Minor changes in / route of backend

--- admin-panel-fullstack/backend/app.js
+++ admin-panel-fullstack/backend/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/admin", admin_router); //  admin routes
 app.use("/api/post", router); // post routes & bulleting routes
 
-app.get("/", (req, res) => res.send("Final Check CI/CD - ✅"));
+app.get("/", (req, res) => res.send("Welcome to SOBF - 🙏"));
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 connectDB();
