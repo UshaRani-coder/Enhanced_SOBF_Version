f721962c222423a4a9b9ef91de547bf60fca9a59
rahuls49 <rahulsahu79998@gmail.com>
2025-01-17 19:45:59 +0530

    Minor changes in / route of backend

--- admin-panel-fullstack/backend/app.js
+++ admin-panel-fullstack/backend/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/admin", admin_router); //  admin routes
 app.use("/api/post", router); // post routes & bulleting routes
 
-app.get("/", (req, res) => res.send("Checking Jenkins Pipeline - ✅"));
+app.get("/", (req, res) => res.send("Final Check CI/CD - ✅"));
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 connectDB();
