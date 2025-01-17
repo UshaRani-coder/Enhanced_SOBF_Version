7fa30b4dedfd0706ccae6fd283cd889738ecc5ea
thecuriousteam <curiousdevelopers.in@gmail.com>
2025-01-17 13:12:30 +0530

    .env fix

--- admin-panel-fullstack/backend/app.js
+++ admin-panel-fullstack/backend/app.js
@@ -1,23 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
 const cors = require("cors");
-const router = require('./routes/post.route');
-const admin_router = require('./routes/admin.route');
-
-
+const router = require("./routes/post.route");
+const admin_router = require("./routes/admin.route");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/admin", admin_router); //  admin routes
+app.use("/api/post", router); // post routes & bulleting routes
 
-app.use('/api/admin', admin_router);  //  admin routes
-app.use('/api/post', router); // post routes & bulleting routes
-
-app.get("/", (req, res) => res.send("Working fine 🙂 Testing CI/CD!"))
+app.get("/", (req, res) => res.send("SOBF Backend Working Fine"));
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
-connectDB()
+connectDB();
