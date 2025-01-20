35cc7c3a948427afbfcd462d4b22ea8d343dac24
thecuriousteam <curiousdevelopers.in@gmail.com>
2025-01-20 10:38:49 +0530

    Cloudinary Error Fix

--- admin-panel-fullstack/backend/middleware/multer.js
+++ admin-panel-fullstack/backend/middleware/multer.js
@@ -1,33 +1,48 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+// const uploadDir = path.join(__dirname, 'uploads');
+// const uploadDir = path.join(process.cwd(), 'uploads');
+const uploadDir = path.join(__dirname, "..", "uploads");
+console.log({ uploadDir });
+
+if (!fs.existsSync(uploadDir)) {
+	fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Configure storage
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${file.fieldname}-${Date.now()}-${file.originalname}`);
-  },
+	destination: (req, file, cb) => {
+		cb(null, uploadDir);
+	},
+	filename: (req, file, cb) => {
+		cb(null, `${file.fieldname}-${Date.now()}-${file.originalname}`);
+	},
 });
-
-
 
 // File filter
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/png', 'image/avif', 'video/mp4', 'video/mkv', 'application/pdf'];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error('Invalid file type'), false);
-  }
+	const allowedTypes = [
+		"image/jpeg",
+		"image/png",
+		"image/avif",
+		"video/mp4",
+		"video/mkv",
+		"application/pdf",
+	];
+	if (allowedTypes.includes(file.mimetype)) {
+		cb(null, true);
+	} else {
+		cb(new Error("Invalid file type"), false);
+	}
 };
 
 // Multer configuration
 const upload = multer({
-  storage,
-  fileFilter,
-  // limits: { fileSize: 10 * 1024 * 1024 }, // 10MB max size
+	storage,
+	fileFilter,
+	// limits: { fileSize: 10 * 1024 * 1024 }, // 10MB max size
 });
 
 module.exports = upload;
