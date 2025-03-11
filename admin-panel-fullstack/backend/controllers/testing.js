3fcf815ba0c1320decd82cbf38ec3029958c3449
Kumari Ranjana Yadav <yranjana757@gmail.com>
2025-03-11 18:20:15 +0530

    added 500 status codde error in testing.js

--- admin-panel-fullstack/backend/controllers/testing.js
+++ admin-panel-fullstack/backend/controllers/testing.js
@@ -1,18 +1,29 @@
 const nodemailer = require("nodemailer");
+const { logger } = require("../middleware/nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === "true",
+  port: "587",
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: "ry648133@gmail.com",
+    pass: "mawb relb avot cukn"
   },
   tls: {
     rejectUnauthorized: false, // Temporary for debugging
   },
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: Number(process.env.SMTP_PORT),
+//   secure: process.env.SMTP_PORT == 465, // Auto-detect
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
 
 console.log("Test email sent successfully:");
 
@@ -30,9 +41,11 @@ async function sendTestEmail(req,res) {
     console.log("SMTP_HOST:", process.env.SMTP_HOST);
     console.log("SMTP_PORT:", process.env.SMTP_PORT);
     console.log("SMTP_USER:", process.env.SMTP_USER);
+
     return res.status(200).json({message:"sent successfully email "})
   } catch (error) {
     console.error("SMTP Error:", error);
+    logger.error("Something went wrong in testing.js" , error)
     return res.status(500).json({ message: error })
   }
 }
