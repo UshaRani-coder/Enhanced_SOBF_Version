fddd48b53dd0e8b0a0607d5e004afef36aa5b0cd
Kumari Ranjana Yadav <yranjana757@gmail.com>
2025-03-11 18:11:09 +0530

    added 500 status codde error in testing.js

--- admin-panel-fullstack/backend/controllers/testing.js
+++ admin-panel-fullstack/backend/controllers/testing.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
+  host: "smtp.gmail.com",
   port: Number(process.env.SMTP_PORT),
   secure: process.env.SMTP_SECURE === "true",
   auth: {
