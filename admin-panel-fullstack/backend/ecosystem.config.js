f38ee63ebf17283ffbbc7fb31be7dc49bec43dd2
Rahul-mandal01 <rahulra16809999@gmail.com>
2025-06-23 17:18:14 +0530

    razorpay key and secret added for production

--- admin-panel-fullstack/backend/ecosystem.config.js
+++ admin-panel-fullstack/backend/ecosystem.config.js
@@ -1,21 +1,39 @@
+
 module.exports = {
   apps: [
     {
       name: 'backend',
+
       script: './app.js',
+
       env: {
         NODE_ENV: 'production',
+
         PORT: process.env.PORT || 5000,
+
         MONGO_URI: process.env.MONGO_URI,
+
         JWT_SECRET: process.env.JWT_SECRET,
+
         CLOUDINARY_API_NAME: process.env.CLOUDINARY_API_NAME,
+
         CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+
         CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+
         SMTP_USER: process.env.SMTP_USER,
+
         SMTP_PASS: process.env.SMTP_PASS,
+
         SMTP_HOST: process.env.SMTP_HOST,
+
         SMTP_PORT: process.env.SMTP_PORT,
-        BASE_URL: "https://backend.sobf.in"
+
+        BASE_URL: 'https://backend.sobf.in',
+
+        RAZORPAY_KEY_ID: 'rzp_live_cqRayxmVI95vLR',
+
+        RAZORPAY_SECRET_ID: 'MXBvgE0ug57ZkjNzrHILUO4b',
       },
     },
   ],
--- admin-panel-fullstack/backend/middleware/razorpayinstance.js
+++ admin-panel-fullstack/backend/middleware/razorpayinstance.js
@@ -1,8 +1,8 @@
 const Razorpay = require('razorpay');
 
 const razorpayInstance = new Razorpay({
- key_id: "rzp_test_3gQIDkPlBpnVfU" ,
- key_secret: "ibAcJb8sCVY3QZlJh7pifO8p"
+ key_id: "rzp_live_cqRayxmVI95vLR" ,
+ key_secret: "MXBvgE0ug57ZkjNzrHILUO4b"
 });
 
 module.exports = razorpayInstance;
